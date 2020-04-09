bd82facc904a18976dd814e529266388b57547a7
Matheus Reis <reis.mmatheus@gmail.com>
2020-04-09 01:55:57 -0300

    Add problem 1003

---
+++ iniciante/1003.cs
@@ -0,0 +1,12 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        var a = int.Parse(Console.ReadLine());
+        var b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("SOMA = {0}", a + b);
+    }
+
+}
