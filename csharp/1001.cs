f3411674e14ad44c94cc064dfb95118bdc2cdfb3
Matheus Reis <reis.mmatheus@gmail.com>
2020-04-08 21:04:49 -0300

    Create 1001.cs

---
+++ csharp/1001.cs
@@ -0,0 +1,13 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        var x = Console.ReadLine();
+        var y = Console.ReadLine();
+        var result = int.Parse(x) + int.Parse(y);
+
+        Console.WriteLine("X = " + result);
+    }
+
+}
