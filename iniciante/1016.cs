eb075e34122128f4651709e11b16ea5be7c91603
Matheus Reis <reis.mmatheus@gmail.com>
2020-04-19 22:57:59 -0300

    Add Problem 1016

---
+++ iniciante/1016.cs
@@ -0,0 +1,10 @@
+using System; 
+
+class URI {
+
+    static void Main(string[] args) { 
+        var entrada = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("{0} minutos", entrada * 2);
+    }
+}
